6a0d90d07944605b25f752435b43f9d2fbf6cda1
Yubunen <wasakon923@gmail.com>
2023-01-12 09:04:57 +0900

    edit readme

---
+++ sample-mysql.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+cnx = mysql.connector.connect(
+    user='root',
+    password='1q2w3e4r5t',
+    host='localhost',
+    port='13306'
+)
+cursor = cnx.cursor()
+cursor.execute('SELECT * FROM lec_db.users')
+
+for id, name in cursor:
+    print(f'{id}: {name}')
+
+cursor.close()
